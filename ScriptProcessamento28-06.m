9f2a164bf9056a5ac02f611b7e981448a3033cbf
Victor Carbón Ferreira <92864869+Vcarbon25@users.noreply.github.com>
2022-09-06 10:28:31 -0300

    Limpei o código para simplificar

--- ScriptProcessamento28-06.m
+++ ScriptProcessamento28-06.m
@@ -1,7 +1,7 @@
 %programa para abrir o csv que o python gerou e obter os vetores uteis para
-%calculo
+%calculoa
 %abrir a tabela do csv
-CSV=readtable('C:\Users\SAMSUNG\Documents\MedidaTCC135graus.csv','NumHeaderLines',1)
+CSV=readtable('C:\Users\SAMSUNG\Documents\MedidaTCC45graus.csv','NumHeaderLines',1)
 %Mudar o nome do arquivo sempre que necessário
 
 %tornar a tabela vetores uteis
@@ -16,25 +16,21 @@ CotZ=table2array(CSV(:,6))
 PunhoX=table2array(CSV(:,7))
 PunhoY=table2array(CSV(:,8))
 PunhoZ=table2array(CSV(:,9))
+%a partir daqui começa o processamento numérico
 
+%para que a coordenada agora esteja em base canonica
+aux=ones(length(OmbroX),1)
+PunhoY=aux-PunhoY
+CotY=aux-CotY
+OmbroY=aux-OmbroY
+%mudando da porcentgem do MP para dimensão em pizels
 PunhoX=PunhoX*640
 PunhoY=PunhoY*480
 CotX=CotX*640
 CotY=CotY*480
 OmbroX=OmbroX*640
 OmbroY=OmbroY*480
-%a partir daqui começa o processamento numérico
-%usando o pensamento do dia 02/09 que basta subtrair a coordenada y de 1
-%oara que a xoordenada agora esteja em base canonica
-aux=ones(length(OmbroX),1)
-disp('coordenadas mediapipe')
-PunhoY
-CotY
-OmbroY
-disp('coordenadas cirrigidas?')
-PunhoY=aux-PunhoY
-CotY=aux-CotY
-OmbroY=aux-OmbroY
+
 % a parte abaixo foi confirmada em testes dia 06/08/2022
 NUM = (OmbroX-CotX).*(PunhoX-CotX)+(OmbroY-CotY).*(PunhoY-CotY)+(OmbroZ-CotZ).*(PunhoZ-CotZ);
 %NUM=sqrt(NUM.^2)
